7b8a5d6e19be641b5218bb7c578bc6f85150e2c2
saniya <saniyaaaaakk@gmail.con>
2024-07-24 16:22:47 +0530

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by SANIYA KHATARKAR on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookWorm/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BookWorm
+//
+//  Created by SANIYA KHATARKAR on 24/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
